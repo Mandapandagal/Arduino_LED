72ee00cd7f0d9a528149e0c1839264a5bff0c14e
Mandapandagal <Mandapandagal@gmail.com>
2017-02-11 10:18:14 -0600

    yay more code

---
+++ We_re_back_in_buisness/We_re_back_in_buisness.ino
@@ -0,0 +1,68 @@
+
+#include <Adafruit_DotStar.h>
+#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+#define NUMPIXELS 8 // Number of LEDs in strip
+// Here's how to control the LEDs from any two pins:
+#define DATAPIN    2
+#define CLOCKPIN   3
+Adafruit_DotStar strip = Adafruit_DotStar(
+                           NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+
+
+void setup() {
+ strip.begin();
+}
+
+int waitTime = 200;
+
+void loop() {
+
+for (int i = 0; i < NUMPIXELS / 2; i++)
+{
+
+   int blue = (i * (240 / ((NUMPIXELS - 2) / 2))) + 15;
+//   int blue = (i * 80) + 15;
+  // strip.Color(blue, green, red);  ==> max 255
+  strip.setPixelColor(i, strip.Color(blue, 0, 0));
+  strip.setPixelColor(NUMPIXELS - i - 1, strip.Color(blue, 0, 0));
+strip.show();
+delay(waitTime);
+}
+delay(waitTime * 5);
+for (int i = 0; i < NUMPIXELS; i++)
+{
+  strip.setPixelColor(i, strip.Color(0, 0, 0));
+}
+strip.show();
+delay(waitTime);
+  }
+
+
+void funnyStuff()
+{
+    
+ for (int i = 0 ; i <= 8 ; i++)
+  {
+    for (int j = 0; j <= 8; j++)
+    {
+
+      if (j == i)
+      {
+        strip.setBrightness(255);
+        strip.setPixelColor (j, 0,  0, 255);
+      }
+      else
+      {
+        strip.setBrightness(255);
+        strip.setPixelColor (j, 0, 255 + j * 8, 0);
+      }
+      
+        strip.show();
+        delay(1);
+
+      }
+    }
+}
+
+
---
+++ we_try_this/we_try_this.ino
@@ -0,0 +1,56 @@
+#include <Adafruit_DotStar.h>
+#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+#define NUMPIXELS 8 // Number of LEDs in strip
+// Here's how to control the LEDs from any two pins:
+#define DATAPIN    2
+#define CLOCKPIN   3
+Adafruit_DotStar strip = Adafruit_DotStar(
+                           NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_RGB);
+
+int colours[7][3] =
+{
+  {255, 0, 0},
+  {200, 140, 0},
+  {255, 221, 0},
+  {8, 255, 0},
+  {0, 255, 0},
+  {255, 0, 255},
+  {255, 255, 255},
+};
+
+void setup() {
+  strip.begin();
+}
+
+
+
+void loop() {
+
+// For every colour...
+  for (int colourIndex = 0; colourIndex < 7; colourIndex ++)
+  {
+    // For every pixel
+    for (int i = 0; i < 8; i++)
+    {
+      // Set the pixel colour
+      strip.setPixelColor(i, colours[colourIndex][0], colours[colourIndex][1], colours[colourIndex][2]);
+    }
+    //    strip.setPixelColor(i, 10, 130, 100);
+    // Show the strip (send output to strip) and wait
+    strip.setBrightness(10);
+    strip.show();
+    delay(100);
+
+// For every pixel...
+//    for (int i = 0; i < 8; i++)
+//    {
+//      // Turn it off (set to black - 0, 0, 0)
+//      strip.setPixelColor(i, 0, 0, 0);
+//    }
+//    strip.show();
+//    delay(50);
+  }
+
+}
+
