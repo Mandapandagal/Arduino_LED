fce6dae9cfc86439230d98d2b665a0260feab7ea
Mandapandagal <Mandapandagal@gmail.com>
2017-02-23 18:43:26 -0600

    ljsgfisEKBf

---
+++ BLINKY/BLINKY.ino
@@ -0,0 +1,32 @@
+#include <Adafruit_DotStar.h>
+#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+#define NUMPIXELS 40 // Number of LEDs in strip
+// Here's how to control the LEDs from any two pins:
+#define DATAPIN    2
+#define CLOCKPIN   3
+Adafruit_DotStar strip = Adafruit_DotStar(
+                           NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_RGB);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  strip.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < NUMPIXELS; i++)
+
+
+  {
+    //bgr color values
+    // WHY?!?!?!
+    strip.setPixelColor(i, 0, 110 ,255);
+
+  
+strip.setBrightness(255);
+    
+  strip.show();
+  }
+}
